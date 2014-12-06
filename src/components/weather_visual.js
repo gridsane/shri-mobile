95c1b9666c75aa931de8c9e9870b772beb478d85
grid <gridsane@gmail.com>
2014-12-06 13:08:28 +0500

    weather-visual: использование weather-fact

--- src/components/weather_visual.js
+++ src/components/weather_visual.js
@@ -1,9 +1,16 @@
 var React = require('react');
+var WeatherFact = require('./weather_fact');
 
 var WeatherVisual = React.createClass({
     render: function () {
         return (
-            <div className="weather-visual">Visual</div>
+            <div className="weather-visual">
+                <WeatherFact
+                    fact={this.props.fact}
+                    fillBackground={false}
+                    showBriefIndicators={false}
+                    showDetailIndicators={false}/>
+            </div>
         );
     }
 });
