8f7d654af764d13f524d27471bb3986abf7dee72
grid <gridsane@gmail.com>
2014-12-06 14:59:40 +0500

    weather-visual: высота столбцов в зависимости от температуры

--- src/components/weather_visual.js
+++ src/components/weather_visual.js
@@ -5,32 +5,34 @@ var dateutils = require('../utils/date');
 var temp2color = require('../utils/temp2color');
 
 var DayColumn = React.createClass({
+    getBarHeightPercent: function () {
+        var height =
+            (50 / (this.props.maxTemp - this.props.minTemp))
+            * (this.props.temp - this.props.minTemp);
+
+        return (50 + height).toFixed(2) + '%';
+    },
+
     render: function () {
-        var temp, weatherIcon;
-        for (var i = this.props.parts.length - 1; i >= 0; i--) {
-            if (this.props.parts[i].type === 'day_short') {
-                temp = this.props.parts[i].temp;
-                weatherIcon = this.props.parts[i].weather_icon;
-                break;
-            }
-        };
-
-
-        var date = new Date(this.props.date);
         var weekdayClasses = React.addons.classSet({
             "weather-visual__column-weekday": true,
-            "weather-visual__column-weekday-weekend": dateutils.isWeekend(date)
+            "weather-visual__column-weekday-weekend": dateutils.isWeekend(this.props.date)
         });
+
+        var barStyle = {
+            backgroundColor: temp2color(this.props.temp),
+            height: this.getBarHeightPercent()
+        }
 
         return (
             <div className="weather-visual__column">
-                <div className="weather-visual__column-bar" style={{backgroundColor: temp2color(temp)}}>
+                <div className="weather-visual__column-bar" style={barStyle}>
                     <div className="weather-visual__column-temp">
-                        {temp}
+                        {this.props.temp}
                     </div>
-                    <WeatherIcon className="weather-visual__column-icon" icon={weatherIcon} />
+                    <WeatherIcon className="weather-visual__column-icon" icon={this.props.weatherIcon} />
                     <div className={weekdayClasses}>
-                        {dateutils.getShortWeekday(date)}
+                        {dateutils.getShortWeekday(this.props.date)}
                     </div>
                 </div>
             </div>
@@ -39,16 +41,44 @@ var DayColumn = React.createClass({
 });
 
 var WeatherVisual = React.createClass({
+
+
     render: function () {
+        var minTemp = null;
+        var maxTemp = null;
+
+        var daysForecast = [];
         var today = dateutils.getToday();
-        var columnCount = 0;
-        var days = this.props.forecast.map(function (forecast) {
-            if (columnCount < 8 && today < dateutils.toDate(forecast.date)) {
-                columnCount++;
-                return <DayColumn {...forecast} />;
+        var count = 0;
+        this.props.forecast.forEach(function (forecast) {
+            var date = dateutils.toDate(forecast.date);
+            if (count > 8 || today >= date) {
+                return;
             }
 
-            return null;
+            for (var i = forecast.parts.length - 1; i >= 0; i--) {
+                if (forecast.parts[i].type === 'day_short') {
+                    var temp = forecast.parts[i].temp
+
+                    minTemp = null === minTemp ? temp : Math.min(minTemp, temp);
+                    maxTemp = null === maxTemp ? temp : Math.max(maxTemp, temp);
+
+                    daysForecast.push({
+                        date: date,
+                        temp: temp,
+                        weatherIcon: forecast.parts[i].weather_icon
+                    });
+                }
+            };
+
+            count++;
+        });
+
+        var days = daysForecast.map(function (forecast) {
+            return <DayColumn
+                    maxTemp={maxTemp}
+                    minTemp={minTemp}
+                    {...forecast} />;
         });
 
         return (
